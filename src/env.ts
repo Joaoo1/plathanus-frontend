403b6aa83a00376200a36b1d12e7d82d3b8ab31b
João <silva.joao1@outlook.com.br>
2025-03-31 15:15:22 -0300

    fix: update API URL for production environment

--- src/env.ts
+++ src/env.ts
@@ -1,5 +1,5 @@
 export const Env = {
 	API_URL: import.meta.env.PROD
-		? 'https://api-plathanus.api.utamo.com.br/api'
+		? 'https://api-plathanus.utamo.com.br/api'
 		: 'http://localhost:3333/api',
 };
