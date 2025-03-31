1f7f61edb59f7bcba1c7454b07b8da3625d324ad
João <silva.joao1@outlook.com.br>
2025-03-31 11:04:04 -0300

    feat: add react-toastify for toast notifications

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import localforage from 'localforage';
+import { ToastContainer } from 'react-toastify';
 import { Router } from './Router';
 import theme from './styles/theme';
+import { toastStyles } from './styles/toast';
 
 localforage.config({
 	name: 'Plathanus frontend',
@@ -14,6 +16,13 @@ const queryClient = new QueryClient();
 export function App() {
 	return (
 		<QueryClientProvider client={queryClient}>
+			<ToastContainer
+				theme="dark"
+				autoClose={3000}
+				position="bottom-center"
+				toastStyle={toastStyles}
+			/>
+
 			<ChakraProvider theme={theme}>
 				<Router />
 			</ChakraProvider>
---
+++ src/styles/toast.ts
@@ -0,0 +1,3 @@
+export const toastStyles: React.CSSProperties = {
+  fontFamily: 'Roboto',
+};
